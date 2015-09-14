e38c4d1f0dd8088c42bacb9cf0208a9613e3c782
Best <kazixma@gmail.com>
2015-09-15 01:16:00 +0700

    login useable

--- app/src/main/java/com/example/bestiize/amhungry/activity/LoginActivity.java
+++ app/src/main/java/com/example/bestiize/amhungry/activity/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.bestiize.amhungry.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.bestiize.amhungry.R;
 
@@ -11,10 +14,20 @@ import com.example.bestiize.amhungry.R;
  * Created by Bestiize on 15/9/2558.
  */
 public class LoginActivity extends AppCompatActivity {
-
+    private Button login;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = (Button)findViewById(R.id.login_button_amhungry);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
 
 
 
--- app/src/main/java/com/example/bestiize/amhungry/activity/MainActivity.java
+++ app/src/main/java/com/example/bestiize/amhungry/activity/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.bestiize.amhungry.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.bestiize.amhungry.R;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
-    private LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
